6d1dfcb1f20c7d10985cc3bbfbcb3da396d4c537
artrayme <artrayme@protonmail.com>
2021-11-28 02:25:20 +0300

    feat: add class-tester

---
+++ testus/testus.h
@@ -0,0 +1,34 @@
+//
+// Created by artrayme on 11/28/21.
+//
+
+#ifndef AMOFS_TESTUS_TESTUS_H_
+#define AMOFS_TESTUS_TESTUS_H_
+
+class Testus {
+
+ public:
+  void testAll();
+
+  void testFilesystemCreation();
+
+  void testFileCreation();
+
+  void testFileDeleting();
+
+  void testFileSearching();
+
+  void testFileMoving();
+
+  void testFileCoping();
+
+  void testFileWritingFromFilesystem();
+
+  void testFileReadingFromFilesystem();
+
+  void testFileWritingFromFile();
+
+  void testFileReadingFromFile();
+};
+
+#endif//AMOFS_TESTUS_TESTUS_H_
