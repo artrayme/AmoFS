931c27cae3b1ad2915410a394b059b4e099895c7
artrayme <artrayme@protonmail.com>
2021-11-28 14:06:01 +0300

    feat: add file creation test

--- testus/testus.cc
+++ testus/testus.cc
@@ -41,7 +41,36 @@ void Testus::testFilesystemCreation() {
 }
 
 void Testus::testFileCreation() {
+  std::cout << "File creation test started" << std::endl;
+
+  size_t blockSize = 10;
+  AmoFS filesystem(blockSize);
+
+  std::string expectedFilename1 = "testFile";
+  filesystem.createFile(expectedFilename1);
+  auto actual1 = filesystem.getFileByName(expectedFilename1);
+  if (actual1->getFilename() == expectedFilename1) {
+    std::cout << testPassedColoring << "Test 1 Passed" << defaultColoring << std::endl;
+  } else {
+    std::cout << testFailedColoring << "Test 1 Failed: "
+              << "expected = " << expectedFilename1 << ", actual1 = " << actual1->getFilename()
+              << defaultColoring << std::endl;
+  }
+
+  std::string nonexistentFile = "I don't exist";
+  auto actual2 = filesystem.getFileByName(nonexistentFile);
+  if (actual2 == nullptr) {
+    std::cout << testPassedColoring << "Test 2 Passed" << defaultColoring << std::endl;
+  } else {
+    std::cout << testFailedColoring << "Test 2 Failed: "
+              << "expected = nullptr" << ", actual1 = " << actual2
+              << defaultColoring << std::endl;
+  }
+
+  std::cout
+      << "File creation test ended" << std::endl;
 }
+
 void Testus::testFileDeleting() {
 }
 void Testus::testFileSearching() {
